8432665e059580008656be3d27f9efeb5cdec477
ThomasDalgaardHavaleska <thh009@edu.zealand.dk>
2026-02-12 13:14:11 +0100

    min

--- Git the fuck up/Program.cs
+++ Git the fuck up/Program.cs
@@ -21,7 +21,7 @@ Console.WriteLine("Vi skal have ny GitMaster");
 Console.WriteLine("bonus contribution");
 
 
-Console.WriteLine("linjen er min");
+Console.WriteLine("linjen er min - Nej det er min!!!!");
 
 
 Console.WriteLine("Jacob - Stop dig selv!");
