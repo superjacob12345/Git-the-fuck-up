636cf2dcbf7dc47c0de3b623573e271382c22398
OniMikki <mik016@edu.zealand.dk>
2026-02-12 12:57:29 +0100

    nu har jeg også skrevet noget

--- Git the fuck up/Program.cs
+++ Git the fuck up/Program.cs
@@ -5,7 +5,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Halløjsa - Jeg har også lavet noget, stoler ikke på jacobs Programmer -skills");
 
 
-
+string mikki = "Huehuehue sejt";
+Console.WriteLine(mikki);
 
 
 Console.WriteLine("Jeg stemmer for at Jakob laver opgaverne");
