43b2a982e7f66a48aa5db7280e5b98bb8dd3a4ee
Lucas Bacher-Nielsen <lub003@edu.zealand.dk>
2026-02-12 13:31:34 +0100

    rød

--- Git the fuck up/Program.cs
+++ Git the fuck up/Program.cs
@@ -33,5 +33,5 @@ Console.WriteLine("jeg langsom");
 
 Console.WriteLine("jeg er for langsom");
 Console.WriteLine("Christian skal slå en streg");
-
+Console.WriteLine("det skal thomas også");
 Console.BackgroundColor = ConsoleColor.Red;
