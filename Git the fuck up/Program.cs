c3bc61bce62409313de27ff2e4bb01ca5985bc64
Lucas Bacher-Nielsen <lub003@edu.zealand.dk>
2026-02-12 12:38:21 +0100

    flag

--- Git the fuck up/Program.cs
+++ Git the fuck up/Program.cs
@@ -1,2 +1,129 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("                       ");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write(" ");
+
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("                       ");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("                       ");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write(" ");
+
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("                       ");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("                       ");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write(" ");
+
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("                       ");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+Console.Write("  ");
+Console.Write("*");
+
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("                       ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("                                       ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("                                       ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("                                       ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("                                       ");
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("                                       ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("                                       ");
+
+Console.BackgroundColor = ConsoleColor.Black;
+
